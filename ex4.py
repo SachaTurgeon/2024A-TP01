78ce91676f0f318ebec94a75a6f66d14d702cf2c
SachaTurgeon <sacha.turgeon@polymtl.ca>
2024-09-11 16:56:34 -0400

    ex_4

--- ex4.py
+++ ex4.py
@@ -3,4 +3,17 @@
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
 
-battery_level = input("Pourcentage de batterie: ")
+battery_level = float(input("Pourcentage de batterie: "))
+
+if (battery_level > 50):
+    distance = (battery_level - 50) * 2 + 25 * 0.5 + 15 + 5 * 2.5 + 5 * 6
+elif (battery_level > 25):
+    distance = (battery_level - 25) * 0.5 + 15 + 5 * 2.5 + 5 * 6
+elif (battery_level > 10):
+    distance = (battery_level - 10) + 5 * 2.5 + 5 * 6
+elif (battery_level > 5):
+    distance = (battery_level - 5) * 2.5 + 5 * 6
+else:
+    distance = battery_level * 6
+
+print(distance)
